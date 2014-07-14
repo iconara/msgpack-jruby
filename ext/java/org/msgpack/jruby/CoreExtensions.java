2a6326b0e9ddae7e1ef2c970a73b5eff29de680a
SATO taichi <ryushi@gmail.com>
2014-07-14 16:56:06 +0900

    add object mutation behavior

--- ext/java/org/msgpack/jruby/CoreExtensions.java
+++ ext/java/org/msgpack/jruby/CoreExtensions.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.jruby.Ruby;
+import org.jruby.RubyArray;
 import org.jruby.RubyClass;
+import org.jruby.RubyIO;
 import org.jruby.RubyModule;
+import org.jruby.RubyString;
 import org.jruby.internal.runtime.methods.CallConfiguration;
 import org.jruby.internal.runtime.methods.DynamicMethod;
 import org.jruby.runtime.Block;
@@ -27,8 +30,26 @@ public class CoreExtensions {
         @Override
         public IRubyObject call(ThreadContext context, IRubyObject self,
             RubyModule clazz, String name, IRubyObject[] args, Block block) {
-          return new Encoder(r).encode(self);
+          RubyString result = (RubyString) new Encoder(r).encode(self);
+          if (0 < args.length) {
+            IRubyObject ro = args[0];
+            if (ro instanceof RubyIO) {
+              RubyIO io = (RubyIO) ro;
+              io.write(context, result);
+              return context.nil;
+            } else if (ro instanceof RubyString) {
+              RubyString rs = (RubyString) ro;
+              rs.cat(result.getByteList());
+              return context.nil;
+            } else if (ro instanceof RubyArray) {
+              RubyArray ra = (RubyArray) ro;
+              ra.append(result);
+              return context.nil;
+            }
+          }
+          return result;
         }
+
         @Override
         public DynamicMethod dup() {
           return this;
